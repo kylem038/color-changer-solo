1325862a81a2a2ce8845247ec64f7df742b552b9
kylem038 <misenckj@gmail.com>
2016-07-18 16:08:12 -0600

    Button functionality working

---
+++ script.js
@@ -0,0 +1,17 @@
+var $randomButtonSubmit = $('.random-color-button');
+var $resetButtonSubmit = $('.reset-color-button');
+
+function getRandomColor () {
+  var hexColor1 = Math.floor(Math.random()*256).toString(16);
+  var hexColor2 = Math.floor(Math.random()*256).toString(16);
+  var hexColor3 = Math.floor(Math.random()*256).toString(16);
+  $('body').css('background-color', "#" + hexColor1 + hexColor2 + hexColor3);
+}
+
+function clearColor () {
+  $('body').css('background-color', "#FFFFFF");
+}
+
+$randomButtonSubmit.on('click', getRandomColor);
+
+$resetButtonSubmit.on('click', clearColor);
